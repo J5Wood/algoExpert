ea4f38859992d0c87b6469a9b3bc0b2e2e2bfd25
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-08-30 20:00:31 -0700

    Completed

--- nonConstructableChange.js
+++ nonConstructableChange.js
@@ -1,3 +1,8 @@
+// Finds first non sumable value given an array of numbers
+
+// Works because:
+    // You start from 0, and just keep adding each consecutinve number in a sorted array to a running total. If the current array value is less than or equal to the running total plus 1, every number below the running total + 1 will be able to be made with a combination of the previous and current numbers in the array. If the current array value is greater than the running total plus 1, that running total plus one can't be made, and is the first unattainable number.
+
 function nonConstructibleChange(coins) {
 	let sortedCoins = coins.sort((a,b) => a - b);
 	let changeStore = 0;
@@ -10,3 +15,7 @@ function nonConstructibleChange(coins) {
 	}
 	return changeStore + 1;
 }
+
+const coins = [5, 7, 1, 1, 2, 3, 22]
+
+console.log(nonConstructibleChange(coins))
