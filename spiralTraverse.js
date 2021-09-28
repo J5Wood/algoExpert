d51dadf607a6ef6f1813c4f22a25f5796aa621a2
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-09-27 22:33:17 -0700

    Completed

--- spiralTraverse.js
+++ spiralTraverse.js
@@ -1,21 +1,30 @@
 function spiralTraverse(array) {
 	let returnArr = []
-	let len = array.length
 	let startRow = 0
-	let endRow = len - 1
+	let endRow = array.length - 1
 	let startCol = 0
-	let endCol = len - 1
-	while(returnArr.length < array.length ** 2){
-		for(let i = startRow; i <= endCol; i++){
+	let endCol = array[0].length - 1
+    
+    function breakCheck() {
+    	if(returnArr.length >= array.length * array[0].length){
+    		return true;
+    	} else { 
+    	    return false;
+    	}
+    }
+	while(returnArr.length < array.length * array[0].length){
+		for(let i = startCol; i <= endCol; i++){
 			returnArr.push(array[startRow][i])
 		}
-		for(let i = startRow + 1; i <= endCol; i++){
+		for(let i = startRow + 1; i <= endRow; i++){
 			returnArr.push(array[i][endCol])
 		}
-		for(let i = endRow - 1; i >= startCol; i--){
+		for(let i = endCol - 1; i >= startCol; i--){
+            if(breakCheck()) break
 			returnArr.push(array[endRow][i])
 		}
 		for(let i = endRow - 1; i > startRow; i--){
+            if(breakCheck()) break
 			returnArr.push(array[i][startCol])
 		}
 		startCol++;
@@ -25,3 +34,9 @@ function spiralTraverse(array) {
 	}
 	return returnArr;
 }
+
+console.log(spiralTraverse([
+    [1, 2, 3, 4],
+    [10, 11, 12, 5],
+    [9, 8, 7, 6]
+  ]))
