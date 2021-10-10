d488d83fe40d0a1b2e556cd0c5ebc5fa6e10a6ae
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-10-09 19:06:55 -0700

    Completed

--- arrayOfProducts.js
+++ arrayOfProducts.js
@@ -13,4 +13,20 @@ function arrayOfProducts(array) {
     return returnArr;
   }
 
+// function arrayOfProducts(array) {
+//     let returnArr = []
+//     let leftNums = []
+//     let rightNums = []
+//     let sum
+//     for(let i = 0; i < array.length; i++){
+//         sum = 1
+//         leftNums = array.slice(0, i)
+//         rightNums = array.slice(i + 1)
+//         leftNums.forEach(num => sum *= num)
+//         rightNums.forEach(num => sum *= num)
+//         returnArr.push(sum)
+//     }
+//     return returnArr
+// }
+
   console.log(arrayOfProducts([5, 1, 4, 2]))
