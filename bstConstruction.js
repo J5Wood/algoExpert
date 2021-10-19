cb5e222e7fb99fce7d80ded7ec0171038924936d
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-10-18 20:38:33 -0700

    Started implementing remove value

--- bstConstruction.js
+++ bstConstruction.js
@@ -45,8 +45,109 @@ class BST {
     }
   
     remove(value) {
-          
-      return this;
+        let prevNode, prevDir, prevBranch, prevBranchDir
+        let currentNode = this
+        let returnNode = null
+
+        // if no more current node, value not found, return null
+        while(currentNode){
+            if(currentNode.value === value){
+            
+                // return node found
+                returnNode = currentNode
+
+
+                // find node for swapping
+                // check right tree left side first
+                // if no right tree, check left tree right side
+
+                if(returnNode.right){
+                    prevBranch = currentNode
+                    prevBranchDir = "right"
+                    currentNode = returnNode.right
+                    
+                    while(currentNode.left){
+                        prevBranch = currentNode
+                        prevBranchDir = "left"
+                        currentNode = currentNode.left
+                    }
+
+                    // if replacement node has right branch, set prev nodes next to that
+                    if(currentNode.right){
+                        if(prevBranchDir === "left"){
+                            prevBranch.left = currentNode.right
+                        } else {
+                            prevBranch.right = currentNode.right
+                        }
+                    }
+
+                    // now that replacement is found
+                    // set previous node's value to swapped node
+                    if(prevNode){
+                        if(prevDir === "right"){
+                            prevNode.right = currentNode
+                        } else{
+                            prevNode.left = currentNode
+                        }
+                    }
+                    
+
+
+                } else if( returnNode.left){
+                    prevBranch = currentNode
+                    prevBranchDir = "left"
+                    currentNode = returnNode.left
+
+                    while(currentNode.right){
+                        prevBranch = currentNode
+                        prevBranchDir = "right"
+                        currentNode = currentNode.right
+                    }
+
+                    // if replacement node has left branch, set prev nodes next to that
+                    if(currentNode.left){
+                        if(prevBranchDir === "right"){
+                            prevBranch.right = currentNode.left
+                        } else {
+                            prevBranch.left = currentNode.left
+                        }
+                    }
+
+
+                     // now that replacement is found
+                    // set previous node's value to swapped node
+                    if(prevNode){
+                        if(prevDir === "right"){
+                            prevNode.right = currentNode
+                        } else{
+                            prevNode.left = currentNode
+                        }
+                    }
+                }
+
+                // set swap nodes nexts to returns
+                currentNode.right = returnNode.right
+                currentNode.left = returnNode.left
+                returnNode.left = null
+                returnNode.right = null
+            
+            } else if(value > currentNode.value){
+                prevNode = currentNode
+                prevDir = "right"
+                currentNode = currentNode.right
+            } else {
+                prevNode = currentNode
+                prevDir = "left"
+                currentNode = currentNode.left
+            }
+        }
+        
+        //getting null for currentNode and this is still 10
+
+        return returnNode
+        // check next nodes, move left node up first, then right
+          // continue until value found or nothing
+          //if nothing found return null
     }
 }
 
@@ -66,4 +167,4 @@ console.log(tree.contains(15))
 console.log(tree.contains(7))
 console.log(tree.contains(12))
 
-// tree.remove(10)
+tree.remove(10)
