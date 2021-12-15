08ae372ca88f850e6dce537bf7150e221382d938
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2021-12-14 23:04:56 -0800

    Cleaner solution

--- nonConstructableChange.js
+++ nonConstructableChange.js
@@ -3,18 +3,31 @@
 // Works because:
     // You start from 0, and just keep adding each consecutinve number in a sorted array to a running total. If the current array value is less than or equal to the running total plus 1, every number below the running total + 1 will be able to be made with a combination of the previous and current numbers in the array. If the current array value is greater than the running total plus 1, that running total plus one can't be made, and is the first unattainable number.
 
+// function nonConstructibleChange(coins) {
+// 	let sortedCoins = coins.sort((a,b) => a - b);
+// 	let changeStore = 0;
+// 	for(let i = 0; i < sortedCoins.length; i++){
+// 		if(sortedCoins[i] > changeStore + 1){
+// 			break;
+// 		} else {
+// 			changeStore += sortedCoins[i];
+// 		}
+// 	}
+// 	return changeStore + 1;
+// }
+
+
 function nonConstructibleChange(coins) {
-	let sortedCoins = coins.sort((a,b) => a - b);
-	let changeStore = 0;
-	for(let i = 0; i < sortedCoins.length; i++){
-		if(sortedCoins[i] > changeStore + 1){
-			break;
-		} else {
-			changeStore += sortedCoins[i];
-		}
-	}
-	return changeStore + 1;
-}
+	coins.sort((a, b) => a - b)
+	  let currentMax = 0
+	  
+	  for(let coin of coins){
+		  if(coin > currentMax + 1) break
+		  currentMax += coin
+	  }
+	return currentMax + 1
+  }
+
 
 const coins = [5, 7, 1, 1, 2, 3, 22]
 
